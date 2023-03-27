306689a0871e5b76d43c8806682ecadcceaa443d
camille-clappaz <camille.clappaz@laplateforme.io>
2023-03-27 15:58:37 +0200

    job 09 OK!

--- job09/script.js
+++ job09/script.js
@@ -1,21 +1,16 @@
-function afficherjourssemaines(jourssemaines) {
-    // let jourssemaines = new Array();
-    // tab[0] = "Lundi";
-    // tab[1] = "Mardi";
-    // tab[2] = "Mercredi";
-    // tab[3] = "Jeudi";
-    // tab[4] = "Vendredi";
-    // tab[5] = "Samedi";
-    // tab[6] = "Dimanche";
-   
-    for (let i = 0; i < longueur; i++) {
-        console.table(tab[i]);
+function tri(numbers, order){
+    if(order == "ASC"){
+        let byValue = (a,b) => a - b;
+        let sorted = numbers.sort(byValue);
+    return console.log(sorted);
     }
-};
+    else{
+        let byValue = (a,b) => a - b;
+        let sorted = numbers.sort(byValue);
+      return  console.log(sorted.reverse());
+    }
+    };
 
-
-// afficherjourssemaines(jourssemaines);
-let tab=new Array("Lundi","Mardi","Mercredi","Jeudi","Vendredi","Samedi","Dimanche");
-let longueur = tab.length;
-
-afficherjourssemaines(tab);
+let numbers = [13,8,2,21,5,1,3,1];
+let order = "ASC"; 
+tri(numbers, order);
