acebe0ee0c167e1c13e176ded8de1bc12a6b929a
camille-clappaz <camille.clappaz@laplateforme.io>
2023-03-31 15:49:58 +0200

    récupere les données du formulaire, recupere l'id et le nom dans le fichier json. Arrive à comparer id et nom formulaire et json. Pour le type c'est encours

--- jour3/job02/script.js
+++ jour3/job02/script.js
@@ -1,19 +1,21 @@
 let images = $('.ordonné').find("img");
+
 $("#melange").on("click", function () {
     images.sort(() => Math.random() - 0.5);
     $('.ordonné').append(images);
 })
 
-for(let i =1; i <= 6; i++){
-    let img= $(`#${i}`); // si je ne crée pas cette variable, append ne comprend pas (`#${i}`)
+for (let i = 1; i <= 6; i++) {
+    const img = $(`#${i}`); // si je ne crée pas cette variable, append ne comprend pas (`#${i}`)
     img.on("click", () => {
-        $(".jeu").append(img);
-        for(let j =1; j <= 6; j++){
-            if (img == $(`#${j}`)){
-                console.log("image bonne place");
-            }
-
-        }
+        $(".jeu").append(img); 
     });
 }
+
+// const images = [];
+// images.push(img);
+// console.log(images);
+
+// IDEE 1:creer un exemple dans une div exemple pour pouvoir comparer avec ma div 2
+// IDEE 2: creer une grosse div avec 6 div dedans, comparer l'id de l'image avec l'id de la div. Du coup img.id1 == div.id1
 
