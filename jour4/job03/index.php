2ca778e262a0d2e3558383d22abb60654a5aa8a5
camille-clappaz <camille.clappaz@laplateforme.io>
2023-03-31 15:50:58 +0200

    id et nom formulaire et json récupérés. Comparaison ok. Comparaison type en cours

--- jour4/job03/index.php
+++ jour4/job03/index.php
@@ -4,9 +4,34 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="../jquery.js"></script>
     <title>Document</title>
 </head>
 <body>
+    <form action="">
+        <input type="text" placeholder="id" id="id"> <br>
+        <input type="text" placeholder="nom" id="nom"><br>
+        <select name="type" id="type">type 
+            <option value="grass">Grass</option>
+            <option value="poison">Poison</option>
+            <option value="fire">Fire</option>
+            <option value="flying">Flying</option>
+            <option value="bug">Bug</option>
+            <option value="normal">Normal</option>
+            <option value="electric">Electric</option>
+            <option value="ground">Ground</option>
+            <option value="fairy">Fairy</option>
+            <option value="fighting">Fighting</option>
+            <option value="psychic">Psychic</option>
+            <option value="rock">Rock</option>
+            <option value="steel">Steel</option>
+            <option value="ice">Ice</option>
+            <option value="ghost">Ghost</option>
+            <option value="water">Water</option>
+            <option value="dragon">Dragon</option>
+       <input type="button" id="button" value="Filtrer"> 
+        </select>
+    </form>
     
 
 <script src="script.js"></script>
--- jour4/job03/script.js
+++ jour4/job03/script.js
@@ -1,15 +1,81 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <script src="../jquery.js"></script>
-    <title>Document</title>
-</head>
-<body>
-    <button id="button">click</button>
+// fetch ('pokemon.json') //recupere un fichier json, ou même texte.
+// .then(res => res.json())
+// .then((data) => data.forEach(element => {
+//     console.log(element.type[0])
 
-<script src="script.js"></script>
-</body>
-</html>
+//     if (element.type[1]){
+//         console.log(element.type[1])
+
+//     }
+// })) // test pour récupérer les types.
+
+// Créez un formulaire permettant de trier ces données.
+// Il doit contenir les champs suivants :
+// ● id (input type text),
+// ● nom (input type text),
+// ● type (liste déroulante <select>)
+// ● filtrer (input type button).
+// Lorsque l’on clique sur “filtrer”, le script doit à l’aide de Fetch, récupérer le contenu du
+// fichier et lister les éléments répondant aux critères sélectionnés en les affichant sur une
+// page HTML.
+
+
+async function getPokemon() {
+    let request = await fetch('pokemon.json'); // on récupère le fichier json, et on affiche l'objet response
+    //    console.log(request);
+    let response = await request.json(); // permet d'avoir directement le data présent dans les then qu'on faisait précedemment.
+    // console.log(response);
+    return response;
+}
+// getPokemon();
+
+
+const filter = document.getElementById("button");
+
+filter.addEventListener("click", async () => {
+    const id = document.getElementById("id");
+    const nom = document.getElementById("nom");
+    const type = document.getElementById("type");
+    let idValue = id.value;
+    let nomValue = nom.value;
+    let typeValue = type.value; // récupère les valeurs du formulaire.
+    let pokemons = await getPokemon();
+    // result = pokemons.filter(function(pokemon){
+    //     console.log(pokemon.name.french);
+    // });
+    // console.log(result);
+    pokemons.forEach(pokemon => {
+        if (idValue == pokemon.id) {
+            let p = document.createElement("p");
+            p.textContent = "id: " + pokemon.id+ " = " + pokemon.name.french;
+            filter.after(p);
+            // console.log(pokemon.name.french); // recupere tout les id 
+        }
+        if (nomValue == pokemon.name.french) {
+            let p = document.createElement("p");
+            p.textContent = pokemon.name.french;
+            filter.after(p);
+        }
+        if (typeValue == pokemon.type[0]) {
+            let p = document.createElement("p");
+            p.textContent = pokemon;
+            filter.after(p);
+        }
+        // console.log(pokemon);
+
+    });
+
+
+
+
+
+
+    // console.log(idValue, nomValue, typeValue, pokemon);
+})
+
+
+
+
+
+
+
