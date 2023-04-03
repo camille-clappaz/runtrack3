ac416290bd2fa0e9d1b0babe8b8648f4c74716f3
camille-clappaz <camille.clappaz@laplateforme.io>
2023-04-03 11:45:06 +0200

    job04OK!

---
+++ jour6/job 04/script.js
@@ -0,0 +1,9 @@
+function myFunction() {
+    var liste = document.getElementById("liste")
+    if(liste.style.display === "none") {
+      liste.style.display = "block";
+    } else {
+      liste.style.display = "none";
+    }
+  } 
+
